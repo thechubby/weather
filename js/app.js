d6bac2f857008fbc772e7796e2340dcb5f4ecb5a
Christina <chubbovsky@yandex.ru>
2018-10-07 00:36:33 +0500

    Nalazhen otvet
    Nalazhen otvet ot promise. Sdelat' vyvod v brauzer

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 class Weather extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { data: null };
+    this.state = { data: null, result: null};
     this.onBtnClick = this.onBtnClick.bind(this);
   }
 
   onBtnClick() {
     const url = "https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
     fetch(url)
-    .then((response)=>{this.state.data = response.json()})
-    console.log(this.state.data)
+    .then((response)=>{return response.json()
+    })
+    .then((value)=>{this.state.data = value
+    })
+    this.result = this.state.data.query.results.channel.astronomy.sunset
+    console.log(this.result)
   }
 
   render() {
     return (<div>
       <button onClick={this.onBtnClick}>Press this button</button>
-      <h1>{this.state.data}</h1>
+      <h1>Рассвет на Гавайях в {this.result}</h1>
     </div>)
   }
 }
