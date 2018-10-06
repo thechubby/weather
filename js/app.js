444ac472662e4fcca26e2d7adbfbd53854a87667
Christina <chubbovsky@yandex.ru>
2018-10-07 00:07:23 +0500

    update
    Bol'she ne voznikaet oshibka pri vyzove funkcii. Dodelat' vyvod

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ class Weather extends React.Component {
   onBtnClick() {
     const url = "https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
     fetch(url)
-    .then(this.state.data = response.json())
+    .then((response)=>{this.state.data = response.json()})
+    console.log(this.state.data)
   }
 
   render() {
